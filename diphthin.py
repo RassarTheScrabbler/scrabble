5f2674f2a0a93288f445d4fbbf83da6e295ea3a6
Simon Chervenak <simonlcherv@gmail.com>
2016-08-30 16:08:47 -0400

    Add files via upload

--- diphthin.py
+++ diphthin.py
@@ -1,8 +1,7 @@
-ospd = open("newDict.txt").read().split("\n") #taken from https://raw.githubusercontent.com/xjtian/PyScrabble/master/wordlists/OSPD4_stripped.txt
+ospd = open("newDict.txt").read().split("\n")
 nospd = []
 for word in ospd:
     nospd.append(word.strip())
-print(len(ospd))
 from itertools import permutations as p
 from string import ascii_uppercase as a_u
 diphths = ["".join(i) for i in p(list(a_u), 2)]
@@ -13,12 +12,12 @@ for i in diphths:
         pass
     dfile = open(i+".txt", "w")
     for word in nospd:
-        #print(word[:2])
         if word[:2] == i:
             pass
             dfile.write(word)
             dfile.write("\n")
     dfile.close()
+    
 
 
 
